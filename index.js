ee47d894a1e897b93a03397049afc512855f679f
dani-ak16 <dannyak16@gmail.com>
2024-11-02 13:35:15 +0100

    v3

--- index.js
+++ index.js
@@ -7,11 +7,64 @@ const nextBtn = document.getElementById("next-btn")
 const decrementBtn = document.getElementById("decrement-btn")
 const incrementBtn = document.getElementById("increment-btn")
 const itemQuantity = document.getElementById("item-quantity")
+const productImage = document.querySelector(".product-image")
+const overlay = document.getElementById("overlay")
+const cartQuantity = document.getElementById("cart-quantity")
+
+const images = [
+    `url(./images/image-product-1.jpg)`,
+    `url(./images/image-product-2.jpg)`,
+    `url(./images/image-product-3.jpg)`,
+    `url(./images/image-product-4.jpg)`
+]
+
+let count = 0
+let quantity = 0
 
 menuBtn.addEventListener("click", () => {
     menuPanel.style.display = "flex"
+    overlay.classList.add("active")
 })
 
 closeBtn.addEventListener("click", () => {
     menuPanel.style.display = "none"
+    overlay.classList.toggle("active")
+})
+
+nextBtn.addEventListener("click", () => {
+    if (count < images.length - 1) {
+        count += 1
+        productImage.style.backgroundImage = `${images[count]}`
+    } else {
+        count = 0
+        productImage.style.backgroundImage = `${images[count]}`
+    }
+})
+
+previousBtn.addEventListener("click", () => {
+    if (count === 0) {
+        count = images.length - 1
+        productImage.style.backgroundImage = `${images[count]}`
+    } else {
+        count -= 1
+        productImage.style.backgroundImage = `${images[count]}`
+    }
+})
+
+incrementBtn.addEventListener("click", () => {
+    quantity += 1
+    itemQuantity.innerText = quantity
+    cartQuantity.innerText = quantity
+})
+
+decrementBtn.addEventListener("click", () => {
+    if (quantity === 0) {
+        itemQuantity.innerText = quantity
+        return
+    } else {
+        quantity -= 1
+        itemQuantity.innerText = quantity
+        cartQuantity.innerText = quantity
+    }
+    
 })
